fa193ba908f6fe0b3f5be1c9795bbec5e7da77ad
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-10-26 01:35:34 +0530

    dock animation done

--- components/system/Taskbar/StyledTaskbar.ts
+++ components/system/Taskbar/StyledTaskbar.ts
@@ -3,9 +3,7 @@ import styled from "styled-components";
 import { TASKBAR_HEIGHT } from "utils/constants";
 
 const StyledTaskbar = styled.nav`
-  backdrop-filter: ${({ theme }) => `blur(${theme.sizes.taskbar.blur})`};
-  border: 1px solid #6c6f7a;
-  background-color: #444652;
+  background-color: transparent;
   bottom: 10px;
   contain: size layout;
   height: ${TASKBAR_HEIGHT}px;
@@ -20,9 +18,6 @@ const StyledTaskbar = styled.nav`
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  padding: 0 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  transition: transform 0.2s;
 `;
 
 export default StyledTaskbar;
--- components/system/Taskbar/index.tsx
+++ components/system/Taskbar/index.tsx
@@ -1,46 +1,22 @@
-import { memo, useCallback } from "react";
-import styled from "styled-components";
+import { memo, useCallback, useRef } from "react";
+import {
+  type MotionValue,
+  m,
+  useMotionValue,
+  useSpring,
+  useTransform,
+} from "framer-motion";
 import StyledTaskbar from "components/system/Taskbar/StyledTaskbar";
 import useTaskbarContextMenu from "components/system/Taskbar/useTaskbarContextMenu";
 import { FOCUSABLE_ELEMENT } from "utils/constants";
 import { useProcesses } from "contexts/process";
-import directory from "contexts/process/directory";
 import { type ProcessArguments } from "contexts/process/types";
 
 const SUGGESTED = ["FileExplorer", "Terminal", "Messenger", "Browser", "Paint"];
 
-const DockButton = styled.button`
-  width: 50px;
-  height: 50px;
-  margin: 0 10px;
-  position: relative; /* Position relative for dot positioning */
-  transition: transform 0.3s ease-in-out;
-  &:hover {
-    transform: scale(1.2);
-  }
-  &:active {
-    transform: scale(1.1);
-  }
-  z-index: 999;
-
-  /* Dot Indicator */
-  &::after {
-    content: "";
-    position: absolute;
-    bottom: 5px; /* Adjust as needed */
-    left: 50%;
-    transform: translateX(-50%);
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    background-color: #0f0; /* Dot color */
-    opacity: ${(props) => (props.isOpen ? 1 : 0)}; /* Only show if open */
-    transition: opacity 0.3s;
-  }
-`;
-
 const Taskbar: FC = () => {
   const { open } = useProcesses();
+  const mouseX = useMotionValue(Infinity);
 
   const openApp = useCallback(
     (pid: string, args?: ProcessArguments) => {
@@ -51,16 +27,51 @@ const Taskbar: FC = () => {
 
   return (
     <StyledTaskbar {...useTaskbarContextMenu()} {...FOCUSABLE_ELEMENT}>
-      {SUGGESTED.map((app) => (
-        <DockButton
-          key={app}
-          onClick={() => openApp(app)}
-          title={directory[app]?.title || app}
-        >
-          <img alt={`${app} icon`} src={`/System/icons/144x144/${app}.avif`} />
-        </DockButton>
-      ))}
+      <m.div
+        className="mx-auto flex h-20 items-end gap-4 rounded-2xl bg-gray-700 px-4 pb-3"
+        onMouseLeave={() => mouseX.set(Infinity)}
+        onMouseMove={(e) => mouseX.set(e.pageX)}
+      >
+        {SUGGESTED.map((app, i) => (
+          <AppIcon
+            key={i}
+            app={app}
+            mouseX={mouseX}
+            openApp={() => openApp(app)}
+          />
+        ))}
+      </m.div>
     </StyledTaskbar>
+  );
+};
+
+const AppIcon = ({
+  mouseX,
+  openApp,
+  app,
+}: {
+  app: string;
+  mouseX: MotionValue;
+  openApp: () => void;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const distance = useTransform(mouseX, (val) => {
+    const bounds = ref.current?.getBoundingClientRect() ?? { width: 0, x: 0 };
+    return val - bounds.x - bounds.width / 2;
+  });
+
+  const widthSync = useTransform(distance, [-150, 0, 150], [60, 100, 60]);
+  const width = useSpring(widthSync, {
+    damping: 12,
+    mass: 0.1,
+    stiffness: 150,
+  });
+
+  return (
+    <m.div ref={ref} className="w-10" onClick={openApp} style={{ width }}>
+      <img alt={`${app} icon`} src={`/System/icons/144x144/${app}.avif`} />
+    </m.div>
   );
 };
 
