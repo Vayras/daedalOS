b43d3d95b4fe70e6f80b63b9b3bb99edd2e6cbf9
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-11-04 13:55:56 +0530

    final 1

---
+++ pages/boot.tsx
@@ -0,0 +1,138 @@
+/* eslint-disable no-useless-return */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Icon } from "@iconify/react";
+
+interface BootProps {
+  restart: boolean;
+  setBooting: (value: boolean | ((prevVar: boolean) => boolean)) => void;
+  sleep: boolean;
+}
+
+export const Boot = ({ restart, setBooting, sleep }: BootProps) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [percent, setPercent] = useState<number>(0);
+
+  useEffect(() => {
+    if (restart && !sleep) {
+      setLoading(true);
+      setPercent(0); // Reset percent when boot starts
+    }
+  }, [restart, sleep]);
+
+  const handleClick = () => {
+    if (sleep) {
+      setBooting(false);
+    } else if (restart || loading) return;
+    else {
+      setLoading(true);
+    }
+  };
+
+  const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const Message = styled.div`
+    transform: translateY(-50%);
+    text-align: center;
+    color: #cccccc;
+
+    @media (min-width: 640px) {
+      margin-top: 24px;
+    }
+  `;
+
+  const ProgressBarContainer = styled.div`
+    position: absolute;
+    top: 50%;
+    inset-x: 0;
+    width: 14rem; /* 56 / 4 = 14rem */
+    height: 0.25rem; /* 1px = 0.25rem */
+
+    @media (min-width: 640px) {
+      height: 0.375rem; /* sm:h-1.5 */
+    }
+
+    background-color: #6b7280; /* bg-gray-500 */
+    border-radius: 0.25rem; /* rounded */
+    overflow: hidden;
+    margin-top: 4rem; /* t-16 = 4rem */
+
+    @media (min-width: 640px) {
+      margin-top: 6rem; /* sm:t-24 = 6rem */
+    }
+
+    margin-inline: auto; /* x-auto */
+  `;
+
+  const ProgressBarFill = styled.span<{ percent?: number }>`
+    position: absolute;
+    top: 0;
+    background-color: white;
+    height: 100%;
+    border-radius: 0.125rem;
+    width: ${(props) => props.percent}%;
+  `;
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout | undefined;
+
+    if (loading) {
+      intervalId = setInterval(() => {
+        setPercent((prev) => {
+          if (prev >= 100) {
+            clearInterval(intervalId);
+            setBooting(false);
+            return 100;
+          }
+          return prev + 1;
+        });
+      }, 30);
+    }
+
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [loading, setBooting]);
+
+  useEffect(() => {
+    if (restart && !sleep) {
+      setLoading(true);
+      setPercent(0);
+    }
+  }, [restart, sleep]);
+
+  return (
+    <Container onClick={handleClick}>
+      <Icon
+        color="white"
+        icon="ic:baseline-apple"
+        style={{ fontSize: "150px" }}
+      />
+      {loading && (
+        <ProgressBarContainer>
+          <ProgressBarFill percent={percent} />
+        </ProgressBarContainer>
+      )}
+      {!restart && !loading && (
+        <Message
+          onClick={() => {
+            setBooting(true);
+          }}
+        >
+          Click to {sleep ? "wake up" : "boot"}
+        </Message>
+      )}
+    </Container>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import { memo } from "react";
-import AppsLoader from "components/system/Apps/AppsLoader";
-import Desktop from "components/system/Desktop";
+import { memo, useState } from "react";
+import { Boot } from "pages/boot";
 import Taskbar from "components/system/Taskbar";
 import useGlobalErrorHandler from "hooks/useGlobalErrorHandler";
 import useGlobalKeyboardShortcuts from "hooks/useGlobalKeyboardShortcuts";
 import useIFrameFocuser from "hooks/useIFrameFocuser";
 import useUrlLoader from "hooks/useUrlLoader";
+import { Login } from "pages/login";
+import AppsLoader from "components/system/Apps/AppsLoader";
+import Desktop from "components/system/Desktop";
 
 const Index = (): React.ReactElement => {
   useIFrameFocuser();
@@ -13,13 +15,55 @@ const Index = (): React.ReactElement => {
   useGlobalKeyboardShortcuts();
   useGlobalErrorHandler();
 
+  const [login, setLogin] = useState(false);
+  const [booting, setBooting] = useState<boolean>(false);
+  const [restart, setRestart] = useState<boolean>(false);
+  const [sleep, setSleep] = useState<boolean>(false);
+
+  const shutMac = (e: React.MouseEvent): void => {
+    e.stopPropagation();
+    setRestart(false);
+    setSleep(false);
+    setLogin(false);
+    setBooting(true);
+  };
+
+  const restartMac = (e: React.MouseEvent): void => {
+    e.stopPropagation();
+    setRestart(true);
+    setSleep(false);
+    setLogin(false);
+    setBooting(true);
+  };
+
+  const sleepMac = (e: React.MouseEvent): void => {
+    e.stopPropagation();
+    setRestart(false);
+    setSleep(true);
+    setLogin(false);
+    setBooting(true);
+  };
+
+  if (booting) {
+    return <Boot restart={restart} setBooting={setBooting} sleep={sleep} />;
+  }
+  if (login) {
+    return (
+      <div>
+        <Desktop>
+          <Taskbar />
+          <AppsLoader />
+        </Desktop>
+      </div>
+    );
+  }
   return (
-    <div>
-      <Desktop>
-        <Taskbar />
-        <AppsLoader />
-      </Desktop>
-    </div>
+    <Login
+      restartMac={restartMac}
+      setLogin={setLogin}
+      shutMac={shutMac}
+      sleepMac={sleepMac}
+    />
   );
 };
 
